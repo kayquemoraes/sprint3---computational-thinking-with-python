5ed3f0d50a4917e7edce01a0308e63d922e5790f
Kayque Moraes <kaymoraesdias@gmail.com>
2023-09-17 23:24:09 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,128 @@
+# EcoSort - Sistema de Reciclagem Inteligente
+
+## Descrição do Projeto
+
+O EcoSort é um sistema de reciclagem inteligente que permite aos usuários depositar resíduos recicláveis em uma lixeira especial. O sistema reconhece os materiais recicláveis com base em códigos de barras e recompensa os usuários com pontos que podem ser trocados por prêmios. Além disso, os usuários podem consultar seu saldo, cadastrar novos produtos no sistema e aprender mais sobre o funcionamento da lixeira inteligente.
+
+## Como Usar o EcoSort
+
+Para utilizar o EcoSort, siga os passos abaixo:
+
+### 1. Iniciar o Programa
+
+Execute o código Python do EcoSort em seu ambiente de desenvolvimento favorito. O programa será iniciado e apresentará um menu com as seguintes opções:
+
+- Entrar
+- Registrar
+
+Digite "1" para entrar no sistema se já tiver uma conta ou "2" para registrar uma nova conta.
+
+### 2. Login ou Registro
+
+#### Se você escolher "Entrar (1)":
+
+- Digite seu nome de usuário e senha quando solicitado.
+- Se as informações estiverem corretas, você fará login com sucesso.
+- Caso contrário, o sistema informará que as credenciais estão incorretas e você poderá tentar novamente ou registrar uma nova conta.
+
+#### Se você escolher "Registrar (2)":
+
+- Digite um nome de usuário e senha para criar uma nova conta.
+- O sistema verificará se o nome de usuário já existe. Se sim, você será solicitado a escolher outro nome de usuário.
+- Após o registro bem-sucedido, você fará login automaticamente.
+
+### 3. Menu Principal
+
+Após o login ou registro, você será redirecionado para o menu principal com as seguintes opções:
+
+- Depositar um resíduo na lixeira
+- Consultar ou cadastrar um produto
+- Consultar seu saldo
+- Conhecer nossa lixeira
+- Encerrar sua sessão
+
+Digite o número correspondente à opção que deseja executar.
+
+### 4. Depositar um Resíduo na Lixeira
+
+- Escolha a opção "Depositar um resíduo na lixeira" no menu.
+- Digite o código de barras do produto a ser descartado.
+- O sistema verificará se o código de barras corresponde a um produto cadastrado.
+- Digite a quantidade do resíduo a ser depositado.
+- O sistema calculará o valor total dos resíduos depositados com base nos preços dos materiais.
+- Os pontos serão adicionados ao seu saldo.
+
+### 5. Consultar ou Cadastrar um Produto
+
+- Escolha a opção "Consultar ou cadastrar um produto" no menu.
+- Digite o código de barras do produto que deseja consultar.
+- O sistema verificará se o produto está cadastrado. Se não estiver, você poderá cadastrá-lo.
+
+### 6. Consultar Seu Saldo
+
+- Escolha a opção "Consultar seu saldo" no menu.
+- Seu saldo atual de pontos será exibido.
+
+### 7. Conhecer Nossa Lixeira
+
+- Escolha a opção "Conhecer nossa lixeira" no menu.
+- Você receberá informações sobre o funcionamento da lixeira inteligente e como você pode ajudar o meio ambiente enquanto ganha recompensas.
+
+### 8. Encerrar Sua Sessão
+
+- Escolha a opção "Encerrar sua sessão" no menu.
+- Sua sessão será encerrada e você será desconectado do sistema.
+
+## Exemplo de Execução
+
+Aqui está um exemplo simulado de como o EcoSort pode ser usado:
+
+```python
+[1] Entrar
+[2] Registrar
+Digite: 2
+
+Nome de usuário: novo_usuario
+Senha: nova_senha
+Registro bem-sucedido!
+
+[1] Depositar um resíduo na lixeira
+[2] Consultar ou cadastrar um produto
+[3] Consultar seu saldo
+[4] Conhecer nossa lixeira
+[5] Encerrar sua sessão
+Digite: 1
+
+Digite o código de barras do produto a ser descartado: 000a
+Digite a quantidade deste resíduo: 3
+Resíduo depositado com sucesso!
+
+[1] Depositar um resíduo na lixeira
+[2] Consultar ou cadastrar um produto
+[3] Consultar seu saldo
+[4] Conhecer nossa lixeira
+[5] Encerrar sua sessão
+Digite: 3
+
+Seu saldo é: 15
+
+[1] Depositar um resíduo na lixeira
+[2] Consultar ou cadastrar um produto
+[3] Consultar seu saldo
+[4] Conhecer nossa lixeira
+[5] Encerrar sua sessão
+Digite: 5
+
+Obrigado por usar nossa lixeira!
+nome de usuario : novo_usuario
+residuos : {'plastico': 3}
+valor total : 15
+
+```
+
+
+## Contribuição
+
+Sinta-se à vontade para contribuir com melhorias neste projeto. Você pode adicionar recursos adicionais, aprimorar a interface do usuário ou fazer correções de bugs.
+
+Esperamos que o EcoSort seja útil para promover a reciclagem e a conscientização ambiental. Faça a sua parte e seja recompensado por suas ações sustentáveis!
---
+++ main.py
@@ -0,0 +1,174 @@
+# Dicionário que armazena informações dos usuários, incluindo seus nomes, senhas e saldos.
+usuarios = {
+    "nomes": [],   # Lista para armazenar os nomes dos usuários.
+    "senhas": [],  # Lista para armazenar as senhas dos usuários.
+    "saldo": []    # Lista para armazenar os saldos dos usuários.
+}
+
+# Dicionário que armazena informações sobre diferentes materiais recicláveis, seus códigos e preços.
+materiais = {
+    "material": ["plastico", "metal", "vidro", "papel"],  # Lista de tipos de materiais recicláveis.
+    "codigo": [["000a", "000c", "0005"], ["000b", "0004"], ["0001", "0002"], ["000d", "0003"]],  # Códigos de barras dos materiais.
+    "preco": [5, 10, 15, 20]  # Lista de preços associados a cada tipo de material.
+}
+
+# Dicionário que armazena informações do cupom, incluindo o nome de usuário, resíduos depositados e o valor total acumulado.
+cupom = {
+    "nome de usuario": "",  # Nome de usuário associado às atividades da sessão.
+    "residuos": {},         # Dicionário para armazenar os resíduos depositados e suas quantidades.
+    "valor total": 0        # Valor total acumulado com base nos resíduos depositados.
+}
+
+# Função para validar a opção escolhida pelo usuário em um menu.
+def validar_opcao(lista, frase):
+    opcao = input(frase)
+    while opcao not in lista:
+        print("Digite uma opção válida.")
+        opcao = input(frase)
+    return opcao
+
+# Função para imprimir os valores de um dicionário de forma recursiva.
+def printa_dicionarios(dic):
+    for key in dic.keys():
+        if type(dic[key]) is dict:
+            printa_dicionarios(dic[key])
+        else:
+            print(f"{key} : {dic[key]}")
+    return
+
+# Função para registrar um novo usuário no sistema.
+def registrar_usuario():
+    while True:
+        novo_usuario = input("Nome de usuário: ").lower()
+        if novo_usuario in usuarios["nomes"]:
+            print(f"{novo_usuario} já existe. Por favor, escolha outro.")
+        else:
+            nova_senha = input("Senha: ")
+            usuarios["nomes"].append(novo_usuario)
+            usuarios["senhas"].append(nova_senha)
+            usuarios["saldo"].append(0)
+            print("Registro bem-sucedido!")
+            break
+    return novo_usuario
+
+# Função para permitir que um usuário faça login no sistema.
+def login_usuario():
+    while True:
+        usuario = input("Nome de usuário: ").lower()
+        senha = input("Senha: ")
+        if usuario in usuarios["nomes"] and senha == usuarios["senhas"][usuarios["nomes"].index(usuario)]:
+            print("Login bem-sucedido!")
+            break
+        else:
+            print("Nome de usuário ou senha incorretos.")
+            opcao = validar_opcao(['1','2'],"[1] Tentar novamente\n[2] Registrar-se\nDigite: ").lower()
+            if opcao == "2":
+                registrar_usuario()
+    return usuario
+
+# Função para permitir que um usuário deposite um resíduo na lixeira.
+def depositar_residuo(usuario):
+    residuo = input("\nDigite o código de barras do produto a ser descartado: ")
+    contem = False
+    for i in range(len(materiais["codigo"])):
+        if residuo in materiais["codigo"][i]:
+            contem = True
+
+    if contem:
+        for i in range(len(materiais["codigo"])):
+            for j in range(len(materiais["codigo"][i])):
+                if residuo == materiais["codigo"][i][j]:
+                    qtd = int(input("Digite a quantidade deste resíduo: "))
+                    material = materiais["material"][i]
+                    if material not in cupom["residuos"].keys():
+                        cupom["residuos"][material] = qtd
+                    else:
+                        cupom["residuos"][material] += qtd
+                    cupom["valor total"] += qtd * materiais["preco"][i]
+                    usuarios["saldo"][usuarios["nomes"].index(usuario)] += qtd * materiais["preco"][i]
+
+        print("Resíduo depositado com sucesso!")
+    else:
+        print('''\nOps! Não encontramos esse produto na nossa base. 
+Verifique se o código foi digitado corretamente, com 4 dígitos. 
+Se sim, acesse a opção [2] para consultá-lo e tente novamente. ''')
+    return
+
+# Função para consultar se um produto está cadastrado na base de dados.
+def consultar_produto():
+    residuo = input("\nDigite o código de barras do produto a ser descartado: ")
+    contem = False
+    for i in range(len(materiais["codigo"])):
+        if residuo in materiais["codigo"][i]:
+            contem = True
+
+    if contem:
+        print("Esse produto já está cadastrado na nossa base!")
+    else:
+        print("Esse produto ainda não está cadastrado no nosso sistema. ")
+        cadastrar = validar_opcao(['1', '2'], "Deseja cadastrar?\n[1] Sim\n[2] Não\nDigite: ")
+        if cadastrar == '1':
+            opcao = validar_opcao(['1','2','3','4'],"[1] Plástico\n[2] Metal\n[3] Vidro\n[4] Papel\nDigite a composição do produto:  ")
+            opcao = int(opcao)
+            cadastrar_produto(residuo, opcao)
+
+# Função para cadastrar um novo produto na base de dados.
+def cadastrar_produto(codigo, composicao):
+    materiais["codigo"][composicao-1].append(codigo)
+    print("Resíduo cadastrado com sucesso!")
+    return
+
+# Função para consultar o saldo de um usuário.
+def consultar_saldo(usuario):
+    print(f'Seu saldo é: {usuarios["saldo"][usuarios["nomes"].index(usuario)]}')
+
+# Função para apresentar informações sobre a lixeira inteligente.
+def conhecer_lixeira():
+    print('''Você já imaginou poder ajudar o meio ambiente e ainda ser recompensado por isso? Pois agora isso é 
+possível com a EcoSort! Para usá-la, é muito simples: basta aproximar o código de barras de uma embalagem cadastrada 
+em nossa base e depositá-la que ela já será reciclada. E o melhor: você ganha prêmios por fazer sua parte!
+
+A EcoSort é capaz de reconhecer qual material foi depositado para separá-lo e garantir que sejam descartados corretamente. 
+Além disso, com o nosso sistema de recompensas, você acumulará pontos que poderão ser trocados por recompensas de nossos 
+parceiros de acordo com a quantidade de itens depositados. Ou seja, quanto mais itens reciclar, mais créditos receberá
+e mais recompensas poderá trocar!
+
+Além disso, caso uma embalagem não esteja registrada no nosso sistema, basta cadastrá-la em nosso site para que nosso
+time faça a validação e cadastre-a na base de dados. Dessa forma, todas as embalagens poderão ser descartadas por aqui.
+Não é demais? 
+
+E não para por aí! Com a lixeira inteligente, você ainda contribui para a construção de um futuro mais sustentável e
+ajuda a preservar o meio ambiente para as próximas gerações. Então, o que está esperando? Faça parte desse movimento 
+pela reciclagem e comece a ser recompensado por suas ações conscientes! ''')
+
+# Solicitar ao usuário que escolha entre fazer login ou registrar uma nova conta.
+opcao = validar_opcao(['1','2'],"[1] Entrar\n[2] Registrar\nDigite: ")
+if opcao == '1':
+    usuario = login_usuario()
+else:
+    usuario = registrar_usuario()
+cupom["nome de usuario"] = usuario
+
+# Loop principal que permite que o usuário realize várias atividades.
+while True:
+    atividade = validar_opcao(["1", "2", "3", "4", "5"], '''\nO que você quer fazer?
+[1] Depositar um resíduo na lixeira
+[2] Consultar ou cadastrar um produto
+[3] Consultar seu saldo
+[4] Conhecer nossa lixeira
+[5] Encerrar sua sessão\nDigite: ''')
+
+    if atividade == "1":
+        depositar_residuo(usuario)
+
+    elif atividade == "2":
+        consultar_produto()
+
+    elif atividade == "3":
+        consultar_saldo(usuario)
+    elif atividade == "4":
+        conhecer_lixeira()
+    else:
+        print("\nObrigado por usar nossa lixeira!")
+        printa_dicionarios(cupom)
+        break
